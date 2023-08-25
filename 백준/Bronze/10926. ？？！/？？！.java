4dfb47034412d53f694d015106739c226cfb63a7
JinBeom Jeong <111486051+beom195@users.noreply.github.com>
2023-08-25 11:42:20 +0900

    [Bronze V] Title: ??!, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,17 +1,12 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String id;
-        String newId;
-        String msg ="??!";
-
-        newId = scanner.nextLine();
-        id = newId;
-        if(id.equals(newId) && id.length() <= 50){
-            System.out.println(id+msg);
-        }
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        String msg = "??!";
+        System.out.println(str+msg);
     }
 }
