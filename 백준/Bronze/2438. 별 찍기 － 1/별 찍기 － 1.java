53e5b6df7897c89d7f952b90145dc85d8d9928e2
JinBeom Jeong <111486051+beom195@users.noreply.github.com>
2023-09-02 14:42:58 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 136 ms, Memory: 14380 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        String result = "";
+
+        for(int i = 1; i <= N; i++){
+            result += "*";
+            System.out.println(result);
+        }
+
+
+        br.close();
+    }
+}
